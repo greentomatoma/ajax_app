dfa49104a351e5a07812b0b96daef36a37754609
greentomatoma <greentomatoma@gmail.com>
2021-04-01 21:48:26 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+  <div class="post">
+    <div class="post-data">
+      投稿日時:${item.created_at}
+    </div>
+    <div calss="post-content">
+      ${item.content}
+    </div>
+  </div>`;
+  return html
+}
+
 function post() {
   // 投稿ボタンの要素を取得
   const submit = document.getElementById("submit");
@@ -12,6 +26,17 @@ function post() {
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      // HTTPステータスによる条件分岐
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 }
 
